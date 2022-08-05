2cd5abdd5d1f3dd0df3e6272e57f10dfc7a49131
Mingyu Kil <90road@gmail.com>
2022-08-05 21:11:24 +0900

    first

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #React Lecture by Nomadcoder
 
-This is the first challenge of the CYPTO TRACKER in lecture "React JS 마스터클래스".
+This is the challenge of the ToDo List in lecture "React JS 마스터클래스".
--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,9 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme= {
-    bgColor: "#2f3640",
-    textColor: "white",
-    accentColor: "#9c88ff",
-    cardBgColor: "transparent",
+    bgColor: "#3F8CF2",
+    cardColor: "white",
+    boardColor: "#DADFE9"
 }
-
-export const lightTheme: DefaultTheme = {
-    bgColor: "whitesmoke",
-    textColor: "black",
-    accentColor: "#9c88ff",
-    cardBgColor: "white",
-  };
 
 
